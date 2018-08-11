2d9c2d7cd3476b79592341c555ec3120f5d8999a
pinfort <ptg@nijitei.com>
2018-08-11 13:42:13 +0900

    ルーム名にデフォルト値を設定しました

--- docs/models.sql
+++ docs/models.sql
@@ -8,7 +8,7 @@ create table Users (
 
 create table Rooms (
     id bigint(13) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(255) COLLATE utf8mb4_bin NOT NULL,
+    `name` VARCHAR(255) COLLATE utf8mb4_bin NOT NULL DEFAULT "",
     created_at datetime default current_timestamp,
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
@@ -26,7 +26,7 @@ create table Talks (
     room_id bigint(13) NOT NULL,
     user_id bigint(13) NOT NULL,
     `text` longtext NOT NULL,
-    created_at datetime default current_timestamp,
+    created_at datetime NOT NULL default current_timestamp,
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(id),
     FOREIGN KEY (room_id) REFERENCES Rooms(id)
