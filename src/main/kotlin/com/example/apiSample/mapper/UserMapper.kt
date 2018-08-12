6f5d5afedf561ede1bad13f1d6c9ce58e3cff9ff
pinfort <ptg@nijitei.com>
2018-08-13 00:45:32 +0900

    ユーザーをuidから見つけられるようにマッパ

--- src/main/kotlin/com/example/apiSample/mapper/UserMapper.kt
+++ src/main/kotlin/com/example/apiSample/mapper/UserMapper.kt
@@ -1,5 +1,6 @@
 package com.example.apiSample.mapper
 
+import com.example.apiSample.model.User
 import com.example.apiSample.model.UserProfile
 import com.example.apiSample.model.UserList
 import org.apache.ibatis.annotations.Mapper
@@ -20,4 +21,11 @@ interface UserMapper {
         """
     )
     fun findBySearchStr(searchStr: String): ArrayList<UserList>
+
+    @Select(
+        """
+        SELECT id, uid, name, created_at, updated_at FROM users WHERE uid = #{uid}
+        """
+    )
+    fun findByUid(uid: String): User
 }
