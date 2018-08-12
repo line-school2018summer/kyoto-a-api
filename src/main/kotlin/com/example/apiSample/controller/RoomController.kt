ae8f108a98e4d3aeaf87fd5719c767febc140e8a
pinfort <ptg@nijitei.com>
2018-08-13 00:47:32 +0900

    トーク取得エンドポイント変更、トーク作成コントローラ追加

--- src/main/kotlin/com/example/apiSample/controller/RoomController.kt
+++ src/main/kotlin/com/example/apiSample/controller/RoomController.kt
@@ -1,20 +1,35 @@
 package com.example.apiSample.controller
 
+import com.example.apiSample.Requests.PostTalkRequest
 import com.example.apiSample.model.Talk
 import com.example.apiSample.model.TalkList
 import com.example.apiSample.service.TalkService
+import com.example.apiSample.service.UserService
+import org.apache.ibatis.javassist.NotFoundException
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
-class RoomController(private val talkService: TalkService) {
+class RoomController(private val talkService: TalkService, private val userService: UserService) {
     @GetMapping(
-            value = ["/rooms/{id}"],
+            value = ["/rooms/{id}/talks"],
             produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)]
     )
     fun getTalks(@PathVariable("id" ) roomId: Long): ArrayList<TalkList> {
-        var Talks: ArrayList<TalkList> = talkService.getTalksFromRoomId(roomId)
+        val Talks: ArrayList<TalkList> = talkService.getTalksFromRoomId(roomId)
         return Talks
+    }
+
+    @PostMapping(
+            value = ["/rooms/{id}/talks"],
+            produces = [(MediaType.APPLICATION_JSON_UTF8_VALUE)]
+    )
+    fun createTalk(@RequestHeader(value="Token", required=true) String token, @PathVariable("id") roomId: Long, @RequestBody request: PostTalkRequest): Talk {
+        val auth = AuthMiddleware()
+        auth.authInit()
+        val uid = auth.verifyIdToken(token) ?: throw UnauthorizedException("")
+        val user = userService.findByUid(uid)
+        val talk: Talk = talkService.createTalk(roomId, user.id, request.text)
     }
 }
