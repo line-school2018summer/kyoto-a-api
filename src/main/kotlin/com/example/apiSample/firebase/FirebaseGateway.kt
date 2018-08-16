da17dff74c813c0e4947fcc95cdb86512a29073b
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-08-16 12:49:21 +0900

    FirebaseGatewayのインスタンスを複数作成できない問題を修正

--- src/main/kotlin/com/example/apiSample/firebase/FirebaseGateway.kt
+++ src/main/kotlin/com/example/apiSample/firebase/FirebaseGateway.kt
@@ -16,18 +16,25 @@ class  FirebaseGateway : AuthGateway{
 
     val logger = LoggerFactory.getLogger(FirebaseGateway::class.java)
 
+    companion object {
+        var isInitialized = false
+    }
+
     init {
-        val firebase_account_path = "/line-summer-kyoto-a-firebase-adminsdk-ei5yu-5a45cf67ce.json"
-        val databaseUrl = "https://line-summer-yoto-a.firebaseio.com/"
+        if (!isInitialized) {
+            val firebase_account_path = "/line-summer-kyoto-a-firebase-adminsdk-ei5yu-5a45cf67ce.json"
+            val databaseUrl = "https://line-summer-yoto-a.firebaseio.com/"
 
-        val firebase_account = FirebaseGateway::class.java.getResourceAsStream(firebase_account_path)
+            val firebase_account = FirebaseGateway::class.java.getResourceAsStream(firebase_account_path)
 
-        val options = FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(firebase_account))
-                .setDatabaseUrl(databaseUrl)
-                .build()
+            val options = FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(firebase_account))
+                    .setDatabaseUrl(databaseUrl)
+                    .build()
 
-        FirebaseApp.initializeApp(options)
+            FirebaseApp.initializeApp(options)
+            isInitialized = true
+        }
     }
 
     /*引数
