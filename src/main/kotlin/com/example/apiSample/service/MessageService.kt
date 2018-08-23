c2c75aca66c106cae44b883bcf9c12ad0284bf2a
pinfort <ptg@nijitei.com>
2018-08-23 23:02:24 +0900

    message getするときに作者の情報を追加して返す

--- src/main/kotlin/com/example/apiSample/service/MessageService.kt
+++ src/main/kotlin/com/example/apiSample/service/MessageService.kt
@@ -3,7 +3,9 @@ package com.example.apiSample.service
 import com.example.apiSample.controller.BadRequestException
 import com.example.apiSample.mapper.MessageMapper
 import com.example.apiSample.model.Message
+import com.example.apiSample.model.MessageForMapping
 import com.example.apiSample.model.MessageList
+import com.example.apiSample.model.NonUidUser
 import org.springframework.stereotype.Service
 
 data class InsertMessage (
@@ -15,14 +17,20 @@ data class InsertMessage (
 
 @Service
 class MessageService(private val messageMapper: MessageMapper) {
-    fun getMessagesFromRoomId(roomId: Long, sinceId: Long = 0, limit: Int = 50): ArrayList<MessageList> {
-        val messages = messageMapper.findByRoomId(roomId, sinceId, limit)
+    fun getMessagesFromRoomId(roomId: Long, sinceId: Long = 0, limit: Int = 50): ArrayList<Message> {
+        val messages_for_mapping = messageMapper.findByRoomId(roomId, sinceId, limit)
+        val mutable_messages = mutableListOf<Message>()
+        for (ms_f_m in messages_for_mapping) {
+            mutable_messages.add(messageFromMessageForMapping(ms_f_m))
+        }
+        val messages: ArrayList<Message> = mutable_messages as ArrayList<Message>
         return messages
     }
 
     fun getMessageFromId(messageId: Long): Message {
-        val message: Message? = messageMapper.findById(messageId)
-        message ?: throw BadRequestException("There are no message")
+        val message_for_mapping: MessageForMapping? = messageMapper.findById(messageId)
+        message_for_mapping ?: throw BadRequestException("There are no message")
+        val message = messageFromMessageForMapping(message_for_mapping)
         return message
     }
 
@@ -52,5 +60,23 @@ class MessageService(private val messageMapper: MessageMapper) {
             return messageMapper.deleteMessage(messageId)
         }
         throw BadRequestException("message creator only can delete message")
+    }
+
+    fun messageFromMessageForMapping(message_for_mapping: MessageForMapping): Message {
+        val user = NonUidUser(
+                id = message_for_mapping.user_id,
+                name = message_for_mapping.user_name,
+                createdAt = message_for_mapping.user_created_at,
+                updatedAt = message_for_mapping.user_updated_at
+        )
+        return Message(
+                id = message_for_mapping.message_id,
+                room_id = message_for_mapping.room_id,
+                user_id = message_for_mapping.user_id,
+                user = user,
+                text = message_for_mapping.text,
+                createdAt = message_for_mapping.message_created_at,
+                updatedAt = message_for_mapping.message_updated_at
+        )
     }
 }
