ac8969e59c922ea0232bc2ff2ed00bbd2c0fd09a
pinfort <ptg@nijitei.com>
2018-08-26 01:32:11 +0900

    joinするのでorder byを指定しないとおかしくなる

--- src/main/kotlin/com/example/apiSample/mapper/MessageMapper.kt
+++ src/main/kotlin/com/example/apiSample/mapper/MessageMapper.kt
@@ -16,7 +16,7 @@ interface MessageMapper {
 
     @Select(
         """
-        SELECT messages.id as message_id, room_id, user_id, `text`, messages.created_at as message_created_at, messages.updated_at as message_updated_at, users.name as user_name, users.created_at as user_created_at, users.updated_at as user_updated_at FROM messages LEFT OUTER JOIN users ON messages.user_id = users.id WHERE room_id=#{roomId} AND messages.id >= #{sinceId} LIMIT #{limit}
+        SELECT messages.id as message_id, room_id, user_id, `text`, messages.created_at as message_created_at, messages.updated_at as message_updated_at, users.name as user_name, users.created_at as user_created_at, users.updated_at as user_updated_at FROM messages LEFT OUTER JOIN users ON messages.user_id = users.id WHERE room_id=#{roomId} AND messages.id >= #{sinceId} ORDER BY message_id LIMIT #{limit}
         """
     )
     fun findByRoomId(roomId: Long, sinceId: Long, limit: Int): ArrayList<MessageForMapping>
