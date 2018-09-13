7ef07f2cba14874a6a83b4f7be50bf6075e3f5ab
pinfort <ptg@nijitei.com>
2018-09-13 10:13:54 +0900

    イベントテーブル追加

--- docs/models.sql
+++ docs/models.sql
@@ -37,3 +37,10 @@ create table messages (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (room_id) REFERENCES rooms(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+create table events (
+    id bigint(13) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    target_id bigint(13) NOT NULL,
+    created_at datetime NOT NULL default current_timestamp,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
