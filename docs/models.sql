1fc806654b56b8e1ee7a89e8b0c95f980c362b50
pinfort <ptg@nijitei.com>
2018-09-13 11:44:29 +0900

    ケツカンマの修正

--- docs/models.sql
+++ docs/models.sql
@@ -43,5 +43,5 @@ create table events (
     event_type int(11) NOT NULL,
     target_id bigint(13) NOT NULL,
     created_at datetime NOT NULL default current_timestamp,
-    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
