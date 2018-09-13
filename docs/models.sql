a359de2620d801b39f38a7e8bc727152a4f88fc2
pinfort <ptg@nijitei.com>
2018-09-13 12:55:02 +0900

    からむをふやした

--- docs/models.sql
+++ docs/models.sql
@@ -41,7 +41,9 @@ create table messages (
 create table events (
     id bigint(13) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     event_type int(11) NOT NULL,
-    target_id bigint(13) NOT NULL,
+    room_id bigint(13) DEFAULT NULL,
+    user_id bigint(13) DEFAULT NULL,
+    message_id bigint(13) DEFAULT NULL,
     created_at datetime NOT NULL default current_timestamp,
     updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
