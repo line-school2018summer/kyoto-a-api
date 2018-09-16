8f1c0156e4c61a55c788eed9f878d9c0dce542ff
pinfort <ptg@nijitei.com>
2018-09-16 15:26:12 +0900

    いったん認証をあきらめる

--- src/main/kotlin/com/example/apiSample/controller/WebsocketController.kt
+++ src/main/kotlin/com/example/apiSample/controller/WebsocketController.kt
@@ -16,21 +16,11 @@ import org.springframework.stereotype.Controller
 import java.util.concurrent.TimeUnit
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 import java.sql.Timestamp
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.authorization.AuthenticatedReactiveAuthorizationManager.authenticated
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 
 @Controller
@@ -49,7 +39,7 @@ class WebsocketController(private val roomService: RoomService,
 
 @EnableWebSocketMessageBroker // WebSocketのメッセージブローカーのBean定義を有効化する
 @Configuration
-class WebSocketConfig : WebSocketMessageBrokerConfigurer, AbstractSecurityWebSocketMessageBrokerConfigurer() { // AbstractWebSocketMessageBrokerConfigurerを継承しWebSocket関連のBean定義をカスタマイズする
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/hello").withSockJS() // WebSocketのエンドポイント (接続時に指定するエンドポイント)を指定
@@ -58,29 +48,5 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer, AbstractSecurityWebSoc
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.setApplicationDestinationPrefixes("/app") // アプリケーション(Controller)でハンドリングするエンドポイントのプレフィックス
         registry.enableSimpleBroker("/topic", "/queue") // Topic(Pub-Sub)とQueue(P2P)を有効化 >>> メッセージブローカーがハンドリングする
-    }
-
-    override fun configureInbound(message: MessageSecurityMetadataSourceRegistry) {
-        message
-                .nullDestMatcher().permitAll()
-                .simpDestMatchers("/app/**").permitAll()
-                .simpSubscribeDestMatchers("/topic/" + "**").permitAll()
-        .anyMessage().permitAll()
-    }
-}
-
-//class CustomUserDetailsService: UserDetailsService {
-//    override fun loadUserByUsername(username: String?): UserDetails {
-//        return User("foo", "password", listOf(SimpleGrantedAuthority("admin")))
-//    }
-//}
-
-@EnableWebSecurity
-class MySecurityConfig: WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/my-login.jsp").permitAll()
-                .anyRequest().permitAll()
     }
 }
